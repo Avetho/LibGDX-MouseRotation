7c9f882e413e438a73a23a5ddbbe251bf69c1fda
matts6206 <matts6206@FHC-IC0034099.wrdsb.ca>
2016-10-18 14:22:18 -0400

    Got rotation working well. Ready to implement into main game.

---
+++ desktop/src/srotate/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package srotate.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import srotate.sRotate;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new sRotate(), config);
+	}
+}
